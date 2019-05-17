9c1f56e5eac18965266113f95027fa1e6f7c20c2
k3yro <zeilenfang@gmail.com>
2019-05-17 09:56:48 +0200

    Rule of five

--- Starter/KopierKonstruktor/Main.cpp
+++ Starter/KopierKonstruktor/Main.cpp
@@ -1,15 +1,20 @@
 #include <string.h>
+#include <chrono>
 
+//Dreierregel: Constructor, Copy Constructor, Destructor
+//Rule of five: Destructor, Copy Constructor, CopyAssigment, Move, MoveAssigment
 class HeapData {
 private:
 	char* data;
 public:
-	//Dreierregel: Constructor, Copy Constructor, Destructor
+	
+	//Konstruktor
 	HeapData() 
 	{
 		data = new char[42];
 	}
 
+	//Kopierkonstruktor
 	HeapData(const HeapData& weitere) 
 	{
 		//shallow copy
@@ -20,9 +25,49 @@ public:
 	}//Bei Zeigern sicherheitshalber immer:
 	//HeapData(const HeapData& weitere) = delete;
 
-	~HeapData() 
+	//Zuweisungsoperator (CopyAssignment)
+	HeapData& operator = (const HeapData& other)
 	{
-		delete data;
+		char* tmp = new char[42];
+		memcpy(tmp, other.data, sizeof(char) * 42);
+		delete[] data;
+		data = tmp;
+		return *this;
+	}
+
+	//MoveKonstruktor
+	HeapData(HeapData&& other)
+	{
+		//Zwei Zeiger umbiegen:
+		data = other.data;
+		other.data = nullptr;
+	}
+
+	//MoveAssignment
+	HeapData& operator=(HeapData&& other)
+	{
+		//Pruefen ob other und this das gleiche Obj sind:
+		if (this != &other)
+		{
+			delete[] data;
+			data = other.data;
+			other.data = nullptr;
+		}
+		return *this;
+	}
+
+	//Bei neuer Klasse immer löschen (sicherheitshalber):
+	/*
+	HeapData(const HeapData&) = delete;
+	HeapData(HeapData&&) = delete;
+	HeapData& operator = (const HeapData&) = delete;
+	HeapData& operator = (HeapData&&) = delete;
+	*/
+
+	//Dekonstruktor
+	virtual/*fuer eventuelle Vererbung*/ ~HeapData() 
+	{
+		delete[] data;
 		data = nullptr;
 
 	}
@@ -31,5 +76,13 @@ public:
 int main() 
 {
 	HeapData data1 = HeapData();
-	HeapData data2 = data1;
+	HeapData data2 = data1; //Benoetigt Kopierkonstruktor
+
+	//Kopierkonstruktor wird nur aufgerufen, wenn anderes Objekt noch nicht existiert! (siehe oben data2)
+	data1 = data2; //Benoetigt Zuweisungsoperator (CopyAssigment)
+
+	HeapData data3 = std::move(data1); //Benoetigt Verschiebungs Konstruktor (Move)
+	data3 = std::move(data2); //Benoetigt MoveAssigment
+
+
 }
